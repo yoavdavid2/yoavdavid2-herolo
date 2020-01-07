94e7100812c4f738d175f4f4a9669f5931961942
yoav.david2 <yoavv.david2@gmail.com>
2020-01-07 20:17:21 +0200

    deploy to github pages

--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,10 +1,9 @@
-const apiKey = 'YuQy0e38cZ8zcLOzLsEoQZALx8mFAVZE';
-//'lB93giYbYMgzTngoCqgk8x0dSVTucyTF'; 
+const apiKey = 'lB93giYbYMgzTngoCqgk8x0dSVTucyTF'; 
  
 export async function requestWeatherDetails(city_code){
   if (city_code !== null && city_code !== undefined) {
     return fetch(
-      `http://dataservice.accuweather.com/currentconditions/v1/${city_code}?apikey=${apiKey}`
+      `https://dataservice.accuweather.com/currentconditions/v1/${city_code}?apikey=${apiKey}`
       ).then((response) => { return response.json() })
     .then((data) => {
       return data;
@@ -17,7 +16,7 @@ export async function requestFiveDayPrediction(city_code) {
   var forecast = null;
   if (city_code !== null && city_code !== undefined) {
     forecast = await fetch(
-      `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${city_code}?apikey=${apiKey}&metric=true`
+      `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${city_code}?apikey=${apiKey}&metric=true`
       ).then((response) => { return response.json() })
     .then((data) => {
       return data;
@@ -30,7 +29,7 @@ export async function getCitiesList(city_name) {
   var new_data = { };
   if (city_name !== null && city_name !== undefined && city_name !== "") {
     new_data = await fetch(
-      `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${city_name}`
+      `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${city_name}`
     ).then((response) => { return response.json() })
     .then((data) => {
       return data.map((city, index) => {
