264e71763b45c0e6bc38000583317e451b8a3b39
yoav.david2 <yoavv.david2@gmail.com>
2020-01-07 21:12:38 +0200

    .

---
+++ build/precache-manifest.388bf7d2b8388826d22b8448d055c505.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b363ca1388cf65ecedd6cee57dd8a92",
+    "url": "/Yoav-David-27-12-2019/index.html"
+  },
+  {
+    "revision": "0435a099f7f38a63f614",
+    "url": "/Yoav-David-27-12-2019/static/css/main.1729fbe4.chunk.css"
+  },
+  {
+    "revision": "84e820db8bd3ff11c1ea",
+    "url": "/Yoav-David-27-12-2019/static/js/2.1245ee9c.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/Yoav-David-27-12-2019/static/js/2.1245ee9c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0435a099f7f38a63f614",
+    "url": "/Yoav-David-27-12-2019/static/js/main.a6ac4aad.chunk.js"
+  },
+  {
+    "revision": "6d99042f5ccb8a06daef",
+    "url": "/Yoav-David-27-12-2019/static/js/runtime-main.e2f9bf24.js"
+  }
+]);
